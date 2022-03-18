3cc907b6928ae10cf04cda74eced7ad064789af5
Arvind-4 <lordarvindthegamer@gmail.com>
2022-03-18 16:47:52 +0530

    Fixed asgi.py

--- web/backend/asgi.py
+++ web/backend/asgi.py
@@ -6,23 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
-
-# BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
-# ENV_FILE_PATH = BASE_DIR / '.env'
-
 import os
-import pathlib
-import dotenv
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+
 import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 
 import apps.video.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
